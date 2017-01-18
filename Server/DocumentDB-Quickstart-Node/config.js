8e5c96fa8cb1a6d6e4766b68ed00ca6e454a5731
Steven Wart <steven.wart@oocl.com>
2017-01-18 08:10:20 -0800

    Removed Server code

--- Server/DocumentDB-Quickstart-Node/config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.host = process.env.HOST || "https://itpocdocdb.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "vKRkqesPqth0wGUBPby609ljKHGNEbKiGIOKPUPIXhGd8WHKDLwrWelCAtGvj1ULqYiKe0sMDVeVPdLJEZab2A==";
-config.databaseId = "ToDoList";
-config.collectionId = "Items";
-
-module.exports = config;
